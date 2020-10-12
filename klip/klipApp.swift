5846d20ae0bf33c659edd9152bc06b9ef7d248a1
Bruno Pimentel <bruno@Brunos-iMac.local>
2020-10-12 15:49:08 -0300

    feat: create project and target

---
+++ klip/klipApp.swift
@@ -0,0 +1,17 @@
+//
+//  klipApp.swift
+//  klip
+//
+//  Created by Bruno Pimentel on 12/10/20.
+//
+
+import SwiftUI
+
+@main
+struct klipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
