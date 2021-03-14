d461e6ccf8789088a9b2b54f4585f0a8366e5f8f
Bruno Pimentel <bruno@pimentel.dev>
2021-03-14 19:35:51 -0300

    feat: put shared content in shared target

---
+++ klipShared/klipShared.h
@@ -0,0 +1,18 @@
+//
+//  klipShared.h
+//  klipShared
+//
+//  Created by Bruno Pimentel on 14/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for klipShared.
+FOUNDATION_EXPORT double klipSharedVersionNumber;
+
+//! Project version string for klipShared.
+FOUNDATION_EXPORT const unsigned char klipSharedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <klipShared/PublicHeader.h>
+
+
